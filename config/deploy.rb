6018500ceb2e51c8a49334fbfc2c3e38aa1b6d0a
Павел Кононенко <storuky@gmail.com>
2015-12-26 19:56:39 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,10 +60,6 @@ namespace :deploy do
     end
   end
 
-  task :remove_layout_cache, :roles => :app do
-    run "bundle exec sidekiq -d"
-  end
-
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
   after  "deploy", "thinking_sphinx:configure"
