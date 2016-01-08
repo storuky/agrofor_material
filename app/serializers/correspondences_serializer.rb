08cc579d5bd0ac69d2097b0bba996c1d135efe2c
Павел Кононенко <storuky@gmail.com>
2016-01-08 21:50:35 +0300

    fix

--- app/serializers/correspondences_serializer.rb
+++ app/serializers/correspondences_serializer.rb
@@ -1,15 +1,14 @@
 class CorrespondencesSerializer < ActiveModel::Serializer
-  attributes :id, :type, :updated_at, :last_message, :new_messages, :users, :positions, :timestamp
+  attributes :id, :type, :updated_at, :last_message, :new_messages, :opponent, :positions, :timestamp
   def positions
     object.position_ids.map do |position_id|
       PositionBase.find_from_cache(position_id, serializer: PositionSerializer)
     end
   end
 
-  def users
-    object.user_ids.map do |user_id|
-      User.find_from_cache(user_id, serializer: UserSerializer)
-    end
+  def opponent
+    user_id = object.user_ids - [scope.current_user.id]
+    User.find_from_cache(user_id[0], serializer: UserSerializer)
   end
 
   def timestamp
