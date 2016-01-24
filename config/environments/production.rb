a12f9b222de99cdd41540657c9a02d9b66279cac
Павел Кононенко <storuky@gmail.com>
2016-01-24 19:45:30 +0300

    absurl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.asset_host = Proc.new { |source, request|
+    if request && request.ssl?
+      "#{request.protocol}#{request.host_with_port}"
+    end
+  }
 end
