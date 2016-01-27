3cd6b4367cde6b8919a31211994015db94b6b7fe
Павел Кононенко <storuky@gmail.com>
2016-01-27 03:07:27 +0300

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,12 +2,16 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :phones, :fullname, :avatar, :language, :currency,
              :website, :skype, :city, :address, :lat, :lng, :company, :currency_id, 
              :additional, :banned, :timezone, :country, :locale, :currency,
-             :first_name, :last_name, :function
+             :first_name, :last_name, :function, :confirmed
 
   has_many :interests
   has_one :avatar, serializer: ImageSerializer
 
   def currency
     object.currency.try(:name)
+  end
+
+  def confirmed
+    !!object.confirmed_at
   end
 end
